0d4701250931ead96672227eaca55f36ea492a57
zmr466 <zmr_233@outlook.com>
2024-03-17 18:08:21 +0800

    1

--- _tmp4.cpp
+++ _tmp4.cpp
@@ -1,3 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector
+typedef long long llg;
+const int MAXN = 10050;
+int prime[MAXN];
+int is_prime[MAXN];
+int Eratosthenes(int n){
+    memset(is_prime,true,sizeof(is_prime));
+    int p=0;
+    is_prime[0]=is_prime[1]=0;
+    for(int i=2;i<=n;i++){
+        if(is_prime[i]){
+            prime[p++]=i;
+            if((llg)i*i<=n)
+                for(int j=i*i; j<=n;j+=i)
+                    is_prime[j]=0;
+        }
+    }
+    return p;
+}
